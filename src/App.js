c6e12babc053b0543b2998b0cb40f28404f297e8
unknown <hobsond26@icloud.com>
2020-04-14 15:26:35 -0400

    Strated on first problm

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ Look at each file to see where you need to pass props or add code
 import React from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
+import Searchbar from './components/SearchBar/SearchBarContainer'
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
   return (
     <div className="App">
       {/* Add imported components here to render them */}
+      < Searchbar />
     </div>
   );
 };
